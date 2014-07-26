6c292823ccd8b1353178debeb9fad9e1b2f21c11
George Georgopoulos <contact@gege.gr>
2014-07-26 13:53:57 +0300

    Introduces users login/register buttons in Homepage.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+	def render_sidebar?
+		satisfy_controller_action_conditions?(
+			'pages' => [:home])
+	end
+
+	def satisfy_controller_action_conditions?(conditions)
+    conditions.any? do |controller, actions|
+      (controller == params[:controller]) && 
+        (actions == :all || actions.include?(params[:action]))
+    end
+  end
 end
